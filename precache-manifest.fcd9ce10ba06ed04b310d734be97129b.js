97b337856f553de1698267e0a17c74bdd35816b1
Andrii <radomely@gmail.com>
2019-07-26 19:51:46 +0300

    Updates

---
+++ precache-manifest.fcd9ce10ba06ed04b310d734be97129b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d3c4d3184561c549848c2921f427b0",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "f02129830fa62be3d384",
+    "url": "/goit-react-hw-02-reader/static/css/main.dd70f427.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/goit-react-hw-02-reader/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "f02129830fa62be3d384",
+    "url": "/goit-react-hw-02-reader/static/js/main.ae0185de.chunk.js"
+  },
+  {
+    "revision": "0074b703c3097bd9a1db",
+    "url": "/goit-react-hw-02-reader/static/js/runtime~main.da19d381.js"
+  }
+]);
